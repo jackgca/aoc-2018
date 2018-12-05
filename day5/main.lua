900491fe5e3a1b83d267e1631f2954e2ba5ff2b8
Jack Graham <contact@jackg.ca>
2018-12-05 13:50:11 -0500

    day 5 part 2

--- day5/main.lua
+++ day5/main.lua
@@ -30,12 +30,39 @@ function part1()
 end
 
 function part2()
-
+    function execute(line)
+        reactions = false
+        for i=1,string.len(line) do
+            first = string.sub(line, i, i)
+            second = string.sub(line, i+1, i+1)
+            if (string.lower(first) == string.lower(second)) then
+                if (first ~= second) then
+                    line = string.gsub(line, first .. second, "")
+                    reactions = true
+                end
+            end
+        end
+        if (reactions == false) then
+            return string.len(line)
+        else
+            return execute(line)
+        end
+    end
+    
+    alpha = "qwertyuiopasdfghjklzxcvbnm"
+    for j,v in pairs(lines) do
+        for i=1,#alpha do
+            letter = string.sub(alpha, i, i)
+            temps = string.gsub(v, "(" .. letter .. ")", "")
+            temps = string.gsub(temps, "(" .. string.upper(letter) .. ")", "")
+            print(execute(temps))
+        end
+    end
 end
 
 function run()
     print("#")
-    helpers.analyze(part1)
+    --helpers.analyze(part1)
     helpers.analyze(part2)
 end
 
--- template.lua
+++ template.lua
@@ -2,6 +2,16 @@
 package.path = package.path .. ";../?.lua"
 helpers = require "helpers"
 
+lines = helpesr.lines_from("example.txt")
+
+function part1()
+
+end
+
+function part2()
+
+end
+
 function run()
     helpers.analyze(part1)
     helpers.analyze(part2)
